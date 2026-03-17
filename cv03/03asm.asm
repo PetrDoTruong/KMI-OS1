f3225edcf849353f3c3a0dadda9acc45552eb026
Truong Giang Do <truonggiang.do01@upol.cz>
2026-03-17 22:17:23 +0100

    cv03

--- cv03/03asm.asm
+++ cv03/03asm.asm
@@ -2,6 +2,11 @@ global inc1
 global rectangle_circumference
 global obsah_obdelniku
 global obvod_ctverce
+global obvod_trojuhelnika
+global obvod_trojuhelnika2
+global obsah_trojuhelnika2
+global objem_krychle
+global avg
 section .text
 ;;
 ;; int inc(int)
@@ -37,7 +42,41 @@ obsah_obdelniku:
 ;;
 ;;
 obvod_ctverce:
-    mov eax, edi ; ulozi do eax hodnotu proměnné 'a'
-    mov ebx, 4   ; uloží do ebx(32b) hodnotu 4
+    mov eax, edi    ; ulozi do eax hodnotu proměnné 'a'
+    mov ebx, 4      ; uloží do ebx(32b) hodnotu 4
     imul eax, ebx   ; vynásobí 
+    ret
+
+obvod_trojuhelnika:
+    mov eax, edi    ; uložíme hodnotu v proměnné a
+    add eax, esi    ; přičteme k 'a' hodnotu v registru esi (proměnná b)
+    add eax, edx    ; přičteme poslední proměnnou v registru edx
+    ret
+
+obvod_trojuhelnika2:
+    mov eax, edi
+    imul eax, 3
+    ret
+
+obsah_trojuhelnika2:
+    mov eax, edi    ; uložíme hodnotu a
+    mov edx, 0      ; vynulujeme zbytek uložený v edx
+    mov ecx, 2      ; uložíme hodnotu pro dělení 2
+    mul esi         ; vynásobíme s hodnotou b
+    div ecx         ; (eax = návratový registr:edx = pro zbytek po dělení) (eax:edx) / registr
+    ret
+    
+objem_krychle:
+    mov eax, edi
+    mul edi
+    mul edi
+    ret
+
+avg:
+    mov eax, edi    ; proměnná a do registru eax
+    add eax, esi    ; přičti proměnnou b
+    add eax, edx    ; přičti proměnnou c
+    mov edx, 0      ; vynuluj před dělením
+    mov ecx, 3      ; ulož 3
+    div ecx         ; vyděl eax/ecx
     ret
--- cv03/03c.c
+++ cv03/03c.c
@@ -3,14 +3,31 @@
 int inc1(int arg);
 int rectangle_circumference(int a, int b);
 int obsah_obdelniku(int a, int b);
-int obvod_ctvertce(int a);
+int obvod_ctverce(int a);
+int obvod_trojuhelnika(int a, int b, int c);
+int obvod_trojuhelnika2(int a);
+int obsah_trojuhelnika2(int a, int b);
+int objem_krychle(int a);
+unsigned int avg(unsigned int a, unsigned int b, unsigned int c);
 
 int main(){
-
+    // inc
     printf("inc(2) je --> %i\n", inc1(2));
+    // rectangle_circumference
     printf("obvod obdelnika o stranach 4x5: %i\n", rectangle_circumference(4, 5));
+    //obsah_obdelniku
     printf("obsah obdelniku o velikosti 2 a 4 je: %i\n", obsah_obdelniku(2, 4));
-    printf("obvod čtverce o velikosti strany A = 5: %i\n", obvod_ctvertce(5));
-    
-	return 0;
+    //obsah_ctverce
+    printf("obvod čtverce o velikosti strany A = 5: %i\n", obvod_ctverce(5));
+    // obvod_trojuhelnika
+    printf("obvod trojuhelnika a=2, b=3, c=2 je: %i\n", obvod_trojuhelnika(2, 3, 2));
+    // obvod_trojuhelnika který je rovnostraný
+    printf("obvod rovnoramenneho trojuhelniku a=3 je: %i\n", obvod_trojuhelnika2(3));
+	// obsah_trojuhelniku
+    printf("obsah pravouhleho trojuhelniku a=3, b=4 je: %i\n", obsah_trojuhelnika2(3, 4));
+    // objem krychle
+    printf("objem krychle a=4 je: %i\n", objem_krychle(4));
+    // avg
+    printf("průměr a=2, b=4, c=9 je: %i\n", avg(2, 4, 9));
+    return 0;
 }
