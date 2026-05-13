12f23bf4f425e80a45b13f22561689dadbe67b53
Truong Giang Do <truonggiang.do01@upol.cz>
2026-05-14 00:00:29 +0200

    OS1

--- random/random.asm
+++ random/random.asm
@@ -1,4 +1,8 @@
 global vypocet
+global aritmetika
+global kombinace
+global divide_and_store
+global ordered
 
 section .text
 
@@ -6,4 +10,40 @@ vypocet:
     mov rax, rdi    ; do rax uložím long a = 2
     add rax, rsi    ; sečtu a + b
     imul rax, rdx   ; vynásobím s rdx
+    ret
+
+aritmetika:
+    mov rax, rdi    ; ulozim hodnotu a
+    sub rax, rsi    ; odečtu b
+    add rax, rdx    ; přičtu c
+    ret
+
+kombinace:
+    mov rax, rdi    ; ulozim promennou a
+    add rax, rsi    ; sečtu a+b
+    add rdx, rcx    ; sečtu c + d = výsledek v rdx
+    sub rax, rdx
+    ret
+
+    ; rdi, rsi, rdx, rcx, r8, r9
+divide_and_store:
+    mov eax, edi    ; ulozime a
+    push rdx        ; schovám si pointer q
+    mov edx, 0      ; vynuluju před dělením
+    idiv esi        ; vydělím a/b, výsledek v eax, zbytek v edx
+
+    mov [rcx], edx  ; na 4. argument kam ukazuje pointer, uložím zbytek
+    pop rdx         ; vrátím pointer ze stacku
+    mov [rdx], eax
+    ret
+
+ordered:
+    mov ax, [rdi]    ; z 1. pointeru uložím hodnotu
+    mov bx, [rsi]    ; z 2. pointeru uložím do bx
+    cmp ax, bx
+    jb b_greater
+    ret
+b_greater:
+    mov word [rdi], bx
+    mov word [rsi], ax
     ret
--- random/random.c
+++ random/random.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 long vypocet(long a, long b, long c);
+long aritmetika(long a, long b, long c);
+long kombinace(long a, long b, long c, long d);
+void divide_and_store(int numerator, int denominator, int *quotiont, int *remainder);
+void ordered(unsigned short *a, unsigned short *b);
 
 int main(){
     // vypocet
     long a = 2;
     long b = 30;
     long c = 20;
-    printf("vypocet pro a=2, b=30, c=20 je: %ld\n", vypocet(a, b, c)); // vysledek = (a+b)*c
-    
+    printf("vysledek = (a+b)*c a=2, b=30, c=20 je: %ld\n", vypocet(a, b, c));
+
+    // aritmetika
+    printf("vysledek = (a-b) + ca=2, b=30, c=20 je: %ld\n", aritmetika(a, b, c));
+
+    // kombinace
+    long d = 21;
+    printf("vysledek = (a+b)-(c+d) a=2, b=30, c=20 d=21 je: %ld\n", kombinace(a, b, c, d));
+
+    // div n store
+    int num = 20;
+    int den = 12;
+    int q = 0;
+    int r = 0;
+
+    divide_and_store(num, den, &q, &r);
+    printf("vysledek: %d / %d = %d, zbytek %d\n", num, den, q, r);
+
+    // oredered
+    unsigned short x = 10;
+    unsigned short y = 20;
+    ordered(&x, &y);
+    printf("x=10, y=20 vysledek (ocekavano 20, 10): a = %hu, b = %hu\n", x, y);
     return 0;
 }
